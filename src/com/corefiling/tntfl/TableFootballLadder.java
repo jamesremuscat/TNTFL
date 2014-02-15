ecd83a2bc18f0f2a807e43da8a9ea8fc36e8e2a5
James Muscat <jamesremuscat@gmail.com>
2014-02-15 20:21:26 +0000

    Make this a constant, so that it's clear what's going on.

--- src/com/corefiling/tntfl/TableFootballLadder.java
+++ src/com/corefiling/tntfl/TableFootballLadder.java
@@ -14,12 +14,13 @@ import com.google.gson.reflect.TypeToken;
 
 public class TableFootballLadder {
 
+  private static final String JSON_EMPTY_LIST = "[]";
   private static final String RECENT_PLAYERS = "recentPlayers";
   private static final String PREFERENCES_KEY = "ladder";
 
   public static List<String> getRecentPlayers(final Context context) {
     final SharedPreferences prefs = context.getSharedPreferences(PREFERENCES_KEY, Context.MODE_PRIVATE);
-    final String value = prefs.getString(RECENT_PLAYERS, "[]");
+    final String value = prefs.getString(RECENT_PLAYERS, JSON_EMPTY_LIST);
 
     final GsonBuilder gsonb = new GsonBuilder();
     final Gson gson = gsonb.create();
