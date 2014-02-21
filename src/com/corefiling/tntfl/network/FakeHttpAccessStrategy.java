5426873dbf428de09c1608777e24dfb034af79ce
James Muscat <jamesremuscat@gmail.com>
2014-02-21 21:21:58 +0000

    Missing close-square-bracket.

--- src/com/corefiling/tntfl/network/FakeHttpAccessStrategy.java
+++ src/com/corefiling/tntfl/network/FakeHttpAccessStrategy.java
@@ -8,6 +8,9 @@ public class FakeHttpAccessStrategy implements HttpAccessStrategy {
     if (url.contains("football.cgi")) {
       return "{\"red\":{\"name\":\"jrem\",\"score\":10},\"blue\":{\"name\":\"aks\",\"score\":0},\"skillChange\":{\"change\":11.38,\"towards\":\"red\"}}";
     }
+    else if (url.contains("mode=recent")) {
+      return "[{\"red\":{\"name\":\"jrem\",\"score\":7,\"skillChange\":2.14038192979},\"blue\":{\"name\":\"esm\",\"score\":3,\"skillChange\":-2.14038192979},\"date\":1392928777},{\"red\":{\"name\":\"jrem\",\"score\":5,\"skillChange\":-0.138448118581},\"blue\":{\"name\":\"cjm\",\"score\":5,\"skillChange\":0.138448118581},\"date\":1392915957}]";
+    }
     else {
       return "{}";
     }
