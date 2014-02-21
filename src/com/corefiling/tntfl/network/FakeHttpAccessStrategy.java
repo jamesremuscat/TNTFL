0aa416f657d0396a9fa72811be79e0f2f9007c50
James Muscat <jamesremuscat@gmail.com>
2014-02-21 21:06:27 +0000

    This is going to have to start dealing with more than just one URL...

--- src/com/corefiling/tntfl/network/FakeHttpAccessStrategy.java
+++ src/com/corefiling/tntfl/network/FakeHttpAccessStrategy.java
@@ -5,7 +5,12 @@ public class FakeHttpAccessStrategy implements HttpAccessStrategy {
 
   @Override
   public String get(final String url) {
-    return "{\"red\":{\"name\":\"jrem\",\"score\":10},\"blue\":{\"name\":\"aks\",\"score\":0},\"skillChange\":{\"change\":11.38,\"towards\":\"red\"}}";
+    if (url.contains("football.cgi")) {
+      return "{\"red\":{\"name\":\"jrem\",\"score\":10},\"blue\":{\"name\":\"aks\",\"score\":0},\"skillChange\":{\"change\":11.38,\"towards\":\"red\"}}";
+    }
+    else {
+      return "{}";
+    }
   }
 
 }
