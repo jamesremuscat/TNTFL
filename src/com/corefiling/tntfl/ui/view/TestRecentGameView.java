608925a51bc7cdb9ada562e193116bc26df39ab8
James Muscat <jamesremuscat@gmail.com>
2014-03-03 21:37:59 +0000

    New test expectations.

--- src/com/corefiling/tntfl/ui/view/TestRecentGameView.java
+++ src/com/corefiling/tntfl/ui/view/TestRecentGameView.java
@@ -11,9 +11,18 @@ public class TestRecentGameView extends TestCase {
   public void testDateFormatting() {
     final Calendar c = Calendar.getInstance();
 
+    assertEquals("Just now", GameDateFormatter.format(c.getTime()));
+
+    c.add(Calendar.MINUTE, -1); // One minute ago
+    assertEquals("1 minute ago", GameDateFormatter.format(c.getTime()));
+    c.add(Calendar.MINUTE, -1); // Two minutes ago
+    assertEquals("2 minutes ago", GameDateFormatter.format(c.getTime()));
+    c.add(Calendar.MINUTE, -57); // 59 minutes ago
+    assertEquals("59 minutes ago", GameDateFormatter.format(c.getTime()));
+    c.add(Calendar.MINUTE, -1); // One hour ago
     assertTrue(GameDateFormatter.format(c.getTime()).matches("[0-9]{2}:[0-9]{2}"));
 
-    c.add(Calendar.DATE, -1);
+    c.add(Calendar.DATE, -1); // Yesterday
     assertTrue(GameDateFormatter.format(c.getTime()).matches("[A-Za-z]{3} [0-9]{2}:[0-9]{2}"));
 
     c.add(Calendar.DATE, -5); // 6 days ago
